b5011d6cf6254107cc7dc78280cd01b339868d2b
Ömer Faruk KESMEZ <31587367+ofke-yazilim@users.noreply.github.com>
2020-10-11 17:44:58 +0300

    web.php üzerinde order yönlendirme  ayarlamaları yapıldı.

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,18 @@ Route::get('/', '\App\Http\Controllers\Frontent\FrontentController@main')->name(
 Route::get('/dashboard', '\App\Http\Controllers\Frontent\FrontentController@index')->name('frontent.index');
 Route::get('/product', '\App\Http\Controllers\Frontent\FrontentController@show')->name('frontent.products.show');
 Route::get('/logout', '\App\Http\Controllers\Frontent\FrontentController@logout')->name('frontent.logout');
-Route::any('/order', '\App\Http\Controllers\Frontent\FrontentController@order')->name('frontent.order');
-Route::any('/orders', '\App\Http\Controllers\Frontent\FrontentController@orders')->name('frontent.orders');
-Route::any('/order/{order}', '\App\Http\Controllers\Frontent\FrontentController@orderShow')->name('frontent.order.show');
+//Route::any('/order', '\App\Http\Controllers\Frontent\FrontentController@order')->name('frontent.order');
+//Route::any('/orders', '\App\Http\Controllers\Frontent\FrontentController@orders')->name('frontent.orders');
+//Route::any('/order/{order}', '\App\Http\Controllers\Frontent\FrontentController@orderShow')->name('frontent.order.show');
 
 Route::middleware([\App\Http\Middleware\CheckUser::class])->prefix('frontent')->name('frontent.')->group(function () {
     Route::any('/basket', '\App\Http\Controllers\Frontent\FrontentController@basket')->name('products.basket');
+});
+
+Route::middleware([\App\Http\Middleware\CheckUser::class])->name('frontent.')->group(function () {
+    Route::any('/order', '\App\Http\Controllers\Frontent\FrontentController@order')->name('order');
+    Route::any('/orders', '\App\Http\Controllers\Frontent\FrontentController@orders')->name('orders');
+    Route::any('/order/{order}', '\App\Http\Controllers\Frontent\FrontentController@orderShow')->name('order.show');
 });
 
 Route::get('/admin/login', '\App\Http\Controllers\Auth\AdminLoginController@showLoginForm')->name('admin.login');
